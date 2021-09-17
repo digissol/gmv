e10a9ddd54c3a97bdbfe1d2accfeeb4b78fea0ba
mohamadousow <mohamadoumoustapha.sow@digissol.com>
2021-09-17 13:21:26 +0000

    mapbox filters

--- web/themes/contrib/cleanplus/reactjs/react-map-gl-cluster/src/App.js
+++ web/themes/contrib/cleanplus/reactjs/react-map-gl-cluster/src/App.js
@@ -80,9 +80,9 @@ export default function App() {
   let realitems = [] // type de realisations projet
   //let urlpartner = base+'/api/taxonomy_term/cateegorie_partenaire';
   let urlpartner = urlbase + 'api/taxonomy_term/cateegorie_partenaire';
-  console.log("urlpartner", urlpartner)
+  //console.log("urlpartner", urlpartner)
   const {data:partners, isPendingpartners, errorpartners} = useFetch(urlpartner);
-  console.log("partners:", partners);
+  //console.log("partners:", partners);
   if(partners){
       partners.data.map((partner) => (
           partitems.push({ value : partner.attributes.drupal_internal__tid, label: partner.attributes.name})
@@ -91,9 +91,9 @@ export default function App() {
 
   //terms taxonomy realisations projets
   let urltypereal = urlbase + 'api/taxonomy_term/realisation';
-  console.log("urltypereal", urltypereal)
+  //console.log("urltypereal", urltypereal)
   const {data:typereals, isPendingtypereals, errortypereals} = useFetch(urltypereal);
-  console.log("typereals:", typereals);
+  //console.log("typereals:", typereals);
   if(typereals){
       typereals.data.map((typereal) => (
           realitems.push({ value : typereal.attributes.drupal_internal__tid, label: typereal.attributes.name})
@@ -102,9 +102,9 @@ export default function App() {
 
   //terms taxonomy thematiques partners
   let urlthema = urlbase + 'api/taxonomy_term/thematique';
-  console.log("urlthema", urlthema)
+  //console.log("urlthema", urlthema)
   const {data:themas, isPendingthemas, errorthemas} = useFetch(urlthema);
-  console.log("themas:", themas);
+  //console.log("themas:", themas);
   if(themas){
       themas.data.map((thema) => (
           themaitems.push({ value : thema.attributes.drupal_internal__tid, label: thema.attributes.name})
@@ -121,13 +121,13 @@ export default function App() {
 
   // concat all strings value for each index found in the array
   const get_full_query = ((tab_query) => {
-    console.log('tab_query', tab_query)
+    //console.log('tab_query', tab_query)
     let tab_values =  []
     Object.keys(tab_query).forEach(function(index) {
-      console.log('value', tab_query[index])
+      //console.log('value', tab_query[index])
       tab_values.push(tab_query[index])
     });
-    console.log(" aaaaaaaaaaaaaaaaaa", tab_values.join('&'))
+    //console.log(" aaaaaaaaaaaaaaaaaa", tab_values.join('&'))
     return tab_values.join('&');
   });
 
@@ -153,7 +153,7 @@ export default function App() {
       tmp_url_full_query_tab['thema_partners'] = null;
       if (selectedOption.value)
         tmp_url_full_query_tab['thema_partners'] = "field_thematiques_target_id=" + selectedOption.value;
-      console.log("field_thematiques_target_id", tmp_url_full_query_tab['thema_partners']);
+      //console.log("field_thematiques_target_id", tmp_url_full_query_tab['thema_partners']);
         
       //new full url
       let full_url = urlbase_geolocations.concat('&', get_full_query(tmp_url_full_query_tab))
@@ -165,40 +165,27 @@ export default function App() {
 
   //type realisations projet
   const handleChangeTypeReal = (selectedOption) => { 
-      console.log("selected multi Option", selectedOption)
+      //console.log("selected multi Option", selectedOption)
       let tmp_url_full_query_tab = url_full_query_tab
       tmp_url_full_query_tab['typereal'] = null;
       let joinquery = []
       if (selectedOption.length)
       {
         let joinquery = selectedOption.map( (elt, index) => {
-        console.log("selected multi Option elt", elt)
+        //console.log("selected multi Option elt", elt)
         return `field_type_realisation_target_id\[`+index + `\]=`+elt.value;
        });
-       console.log("selected multi Option elt", joinquery);
+       //console.log("selected multi Option elt", joinquery);
        tmp_url_full_query_tab['typereal'] = joinquery.join('&');
        //setselectedOptionTypeReal(selected);  
       }  
-      console.log("url typereal", tmp_url_full_query_tab) 
+      //console.log("url typereal", tmp_url_full_query_tab) 
 
       //new full url
       let full_url = urlbase_geolocations.concat('&', get_full_query(tmp_url_full_query_tab))
       setUrl(full_url);
       seturl_full_query_tab(tmp_url_full_query_tab)
       console.log("full_url", full_url) 
-  };
-
-  const handleSubmit = () => {
-    // the item selected
-    /*//new url
-    "&partenaire_categorie=" + selectedOption;
-    
-    console.log("new url", url);
-    setUrl(url);*/
-  }
-  const handleNameChange = (e) => {
-    const keyword = e.target.value;
-    setName(keyword);
   };
 
   const Option = (props) => {
@@ -216,7 +203,53 @@ export default function App() {
   );
 };
 
-    // all geolocations
+  //get all title values (from partners or from projet)
+  let title_items = []
+
+  const [global_title_items, setglobal_title_items] = useState([]) //in order to store the global array of titles (from partner and project)
+
+  const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  //after choosing a title of partner or project from the autocompletion search box
+  const handleOnSelect = (keyword) => {
+    // the keyword selected
+    //console.log("handleOnSelect keyword", keyword)
+
+    //setName(keyword);
+    let tmp_url_full_query_tab = url_full_query_tab
+    tmp_url_full_query_tab['keyword_search'] = null;
+    if (keyword && keyword.name) {
+      tmp_url_full_query_tab['keyword_search'] = "combined_field_title=" + encodeURIComponent(keyword.name)
+      console.log("combined_field_title", tmp_url_full_query_tab['keyword_search']);
+        
+      //new full url
+      let full_url = urlbase_geolocations.concat('&', get_full_query(tmp_url_full_query_tab))
+      setUrl(full_url);
+      console.log("full_url", full_url)
+      seturl_full_query_tab(tmp_url_full_query_tab)
+      //setselectedOptionThema(selectedOption);
+    }
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  const formatResult = (item) => {
+    return item;
+   // return (<p dangerouslySetInnerHTML={{__html: '<strong>'+item+'</strong>'}}></p>); //To format result as html
+  }
+
+  // all geolocations
   var glocations_restruct = [];
   var glocations_restruct_no_duplicate_id = []; //avoid duplicata
   const [gLocations, setGLocations] = useState([]);
@@ -225,7 +258,7 @@ export default function App() {
       .then(res => res.json())
       .then(
         (result) => {
-          console.log("new url for looping", url);
+          //console.log("new url for looping", url);
           result.slice(0, 2000).forEach((point) => 
             { 
               if (point.projet_id && !glocations_restruct_no_duplicate_id.includes(point.projet_id))//in case in a project share between many partenaires..
@@ -243,6 +276,10 @@ export default function App() {
                       ]
                     }
                   }); 
+
+                  //to get after also all titles(from partner or project) for an eventual likely autocomplete search
+                  title_items.push({id: point.projet_id, name: point.projet_title})
+                  //console.log("title_items", title_items);
 
                   glocations_restruct_no_duplicate_id.push(point.projet_id);                
               }
@@ -263,6 +300,10 @@ export default function App() {
                     }
                   });
 
+                  //to get after also all titles(from partner or project) for an eventual likely autocomplete search
+                  title_items.push({id: point.partenaire_id, name: point.partenaire_title})
+                  //console.log("title_items", title_items);
+
                   glocations_restruct_no_duplicate_id.push(point.partenaire_id);
               }
                               
@@ -271,6 +312,13 @@ export default function App() {
 
           setGLocations(glocations_restruct);
           console.log("gLocations.length", glocations_restruct);
+
+          if (!global_title_items.length && title_items.length){
+            setglobal_title_items(title_items) 
+            console.log('global_title_items', global_title_items)
+          }
+          else
+            console.log('hhhhhhhhhhhhh', global_title_items)
         }
       )
       .catch(
@@ -313,26 +361,42 @@ return (
                   options={realitems}
                   className="basic-multi-select"
                   classNamePrefix="select"
-                  placeholder="Sélectionner"
-                
-                  
-                 
-                  onChange={handleChangeTypeReal}
-                 
+                  placeholder="Sélectionner"              
+                  onChange={handleChangeTypeReal}               
                   />
               </div>
               <div className="col-md-3">
-                <input
-                    type="search"
-                    value={name}
-                    //onChange={handleNameChange}
-                    className="input"
-                    placeholder="Saisir un mot.."
-                />            
+                <div className="App">
+                  <header className="App-header">
+                    <div style={{ width: 400 }}>
+                      <ReactSearchAutocomplete
+                        items={global_title_items}
+                        fuseOptions={{
+                          shouldSort: true,
+                          threshold: 0,
+                          location: 0,
+                          distance: 100,
+                          maxPatternLength: 32,
+                          minMatchCharLength: 2,
+                          keys: [
+                            "name",
+                            "id",
+                          ],
+                        }}
+                        onSearch={handleOnSearch}
+                        onHover={handleOnHover}
+                        onSelect={handleOnSelect}
+                        onFocus={handleOnFocus}
+                        autoFocus
+                        formatResult={formatResult}
+                        placeholder="Saisir au moins 2 lettres"
+                      />
+                    </div>
+                  </header>
+                </div>       
               </div>
-              <div className="col-md-3">
-                  <input type="submit" value="Chercher" onSubmit={handleSubmit} />
-              </div>
+
+             
 
     </div>
     <div className="row the_mapbox">
@@ -349,46 +413,47 @@ return (
       >
         <Cluster radius={80} extent={512} nodeSize={64} component={ClusterMarker}>
           {
-             
                 gLocations.map((point) => { 
                  
-                  //console.log("id", point.id);
+                //console.log("id", point.id);
 
-                  if (point.type == 'partenaire')
-                  {
-                     return(                 
-                      <Marker
-                        key={point.id}
-                        longitude={point.geometry.coordinates[1]}
-                        latitude={point.geometry.coordinates[0]}
-                      >
-                       <button className={point.type}>
-                           <FaPiedPiperPp color="#aaD100" size='2em' title={point.title}/>
-                        </button>
+                if (point.type == 'partenaire')
+                {
+                   return(                 
+                    <Marker
+                      key={point.id}
+                      longitude={point.geometry.coordinates[1]}
+                      latitude={point.geometry.coordinates[0]}
+                    >
+                     <button className={point.type}>
+                         <FaPiedPiperPp color="#aaD100" size='2em' title={point.title}/>
+                      </button>
 
-                      </Marker>        
-                    );
-                  }
-
-                  if (point.type == 'projet')
-                  {           
-                     return(
-                      
-                      <Marker
-                        key={point.id}
-                        longitude={point.geometry.coordinates[1]}
-                        latitude={point.geometry.coordinates[0]}
-                      >
-                        <button style={{outline: 'none !important;'}} className={point.type}>
-                           <FaProductHunt color="#00D100" size='1.6em' title={point.title}/>
-                        </button>
-                      </Marker>
-                    );
-                  }
+                    </Marker>        
+                  );
                 }
-              ) 
+
+                if (point.type == 'projet')
+                {           
+                   return(
+                    
+                    <Marker
+                      key={point.id}
+                      longitude={point.geometry.coordinates[1]}
+                      latitude={point.geometry.coordinates[0]}
+                    >
+                      <button style={{outline: 'none !important;'}} className={point.type}>
+                         <FaProductHunt color="#00D100" size='1.6em' title={point.title}/>
+                      </button>
+                    </Marker>
+                  );
+                }
+              }
+            )
+  
          }
         </Cluster>
+       
       </MapGL>
      </div> 
     </div> 
